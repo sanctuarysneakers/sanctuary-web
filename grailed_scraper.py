e50e467387f81d58dfcc510c50cd90062d2e8a7b
EricHasegawa <eric.hasegawa@mail.utoronto.ca>
2020-06-23 17:52:42 -0600

    worked on finding image urls

--- grailed_scraper.py
+++ grailed_scraper.py
@@ -1,5 +1,9 @@
 import time
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from Sneaker import Sneaker
 from selenium.webdriver.chrome.options import Options
 
@@ -28,21 +32,23 @@ def get_sneakers():
     while True and len(sneakers) < sneaker_count:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
         # Get sneakers currently on page and add to sneakers list
         feed = driver.find_elements_by_class_name('feed-item')
+        images = WebDriverWait(driver, 10).until(
+            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".feed-item .listing-cover-photo>img")))
+        print(len(images))
+        for image in images:
+            print(image.get_attribute("src"))
         for item in feed:
             info = item.get_attribute('innerHTML').split(">")
-            if len(info) > 1:
-                img_url = info[5]
-                print(info)
-                print(img_url)
-                item_url = info[0]
-                item_url = "https://www.grailed.com" + item_url[9:-43]
-                split = item.text.strip().split('\n')
-                if len(split) > 3:
-                    sneaker = Sneaker(split[1], split[2], split[3], split[4], item_url, img_url)
-                    sneakers.append(sneaker)
+            item_url = info[0]
+            item_url = "https://www.grailed.com" + item_url[9:-43]
+
+            # Create Sneaker object for this pair of shoes
+            split = item.text.strip().split('\n')
+            if len(split) > 3:
+                sneaker = Sneaker(split[1], split[2], split[3], split[4], item_url, None)
+                sneakers.append(sneaker)
         # Wait to load page
         time.sleep(0.5)
 
