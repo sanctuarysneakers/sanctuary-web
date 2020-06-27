a983b50796990ae8fed407038f4bc83521e6d537
EricHasegawa <eric.hasegawa@mail.utoronto.ca>
2020-06-27 16:44:11 -0600

    Working on images bug in grailed_scraper_test.py

--- grailed_scraper.py
+++ grailed_scraper.py
@@ -3,7 +3,7 @@ import urllib3
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from Sneaker import Sneaker
 from selenium.webdriver.chrome.options import Options
--- grailed_scraper_test.py
+++ grailed_scraper_test.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+
 import time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 driver.get('https://www.grailed.com/designers/jordan-brand/hi-top-sneakers')
 
 
@@ -17,30 +22,37 @@ def get_sneakers():
     
     # Amount of scrolls to the bottom of page
     scrolls = 10
+    sneakers = []
+    while len(sneakers) < 22378:
+        i = 0
+        while i < scrolls:
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    i = 0
-    while (i < scrolls):
-        # Scroll down to bottom
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
 
-        # Wait to load page
-        time.sleep(SCROLL_PAUSE_TIME)
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
 
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if (new_height == last_height):
-            break
-        last_height = new_height
+            i += 1
 
-        i += 1
+        time.sleep(2)
 
-    time.sleep(2)
+        # Get all the shoe html items (there are about 40 per page scroll)
+        feed = driver.find_elements(By.CSS_SELECTOR, 'div.feed-item')
+        shoe_images = driver.find_elements(By.CSS_SELECTOR, 'div.listing-cover-photo img')
+        for item in feed:
+            img_div = item.find_element_by_class_name('listing-cover-photo ')
+            img = WebDriverWait(img_div, 10).until(
+                ec.visibility_of_element_located((By.TAG_NAME, 'img')))
+            print(img.get_attribute('src'))
+        # Print the number of shoes
+        print(len(feed), len(shoe_images))
 
-    # Get all the shoe html items (there are about 40 per page scroll)
-    shoe_items = driver.find_elements(By.CSS_SELECTOR, 'div.feed-item')
-
-    # Print the number of shoes
-    print(len(shoe_items))
 
 get_sneakers()
 
