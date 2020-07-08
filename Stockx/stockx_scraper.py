c19a695d89ae82b20ad557a1b2728ccc1500b0d0
EricHasegawa <eric.hasegawa@mail.utoronto.ca>
2020-07-08 15:59:40 -0600

    Completed initial stockx model

--- Stockx/stockx_scraper.py
+++ Stockx/stockx_scraper.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
@@ -8,19 +7,21 @@ import time
 options = Options()
 options.add_argument("--start-maximized")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-driver.get('https://stockx.com/retro-jordans')
+
+
+def get_item_info(item):
+    info = {'brand': 'Air Jordan', 'url': 'https://stockx.com' + item.find('a')['href'], 'size': 'Multiple sizes',
+            'model': item.find('div', {'class': 'PrimaryText-m2st9e-0 hugwvm'}).text,
+            'price': item.find('div', {'class': 'PrimaryText-m2st9e-0 feUFro'}).text, 'img': 'Lazy loaded'}
+    return info
 
 
 def insert_items(feed):
     for item in feed:
-        print(item)
-        info = {}
-        info['brand'] = 'Air Jordan'
-        info['url'] = 'https://stockx.com/' + item['href']
-        print("INFO", info['url'])
+        print(get_item_info(item))
 
 
-def verify_human(page_html, soup):
+def verify_human():
     print("They detected we are using automated software")
 
 
@@ -34,7 +35,7 @@ def run_scraper():
         time.sleep(1.0)
         automation_check = soup.find('title').text
         if automation_check == 'Access to this page has been denied.':
-            verify_human(page_html, soup)
+            verify_human()
         feed = soup.find_all('div', {'class': 'tile browse-tile updated'})
         insert_items(feed)
         time.sleep(1.0)
