e3f77384de84845691c67040bb9f947377ef3ed4
EricHasegawa <eric.hasegawa@mail.utoronto.ca>
2020-07-26 16:34:46 -0700

    More progress on API

--- Flask/restful_api.py
+++ Flask/restful_api.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify
 from flask_restful import Resource, Api
+import mysql.connector
+from mysql.connector import ProgrammingError
+
+host = "test-database-1.cmamugrum56i.us-west-2.rds.amazonaws.com"
+user = "admin"
+passwd = "password"
+db_name = "sneakers"
+try:
+    conn = mysql.connector.connect(host=host, user=user, passwd=passwd, database=db_name)
+    c = conn.cursor()
+except ProgrammingError:
+    print("couldn't connect to database")
+c.execute("USE sneakers;")
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,7 +22,9 @@ class Search(Resource):
 
     @staticmethod
     def get(user_input):
-        return {'Sneaker': user_input}
+        query = "SELECT * FROM sneakers WHERE url == %s"
+        c.execute(query, user_input)
+        return jsonify(c)
 
 
 api.add_resource(Search, '/search<string:input>')
