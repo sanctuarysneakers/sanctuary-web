a44ff68e8c2be7a48e4f916db10a78bff29c5e70
Jason Thomo <amjbvic@gmail.com>
2020-08-01 12:55:47 -0700

    added model searching parameter

--- Flask/restful_api.py
+++ Flask/restful_api.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 import mysql.connector
 from mysql.connector import ProgrammingError
 
-
 def connect_to_db():
     host = "test-database-1.cmamugrum56i.us-west-2.rds.amazonaws.com"
     user = "admin"
@@ -27,7 +26,8 @@ cors = CORS(application, resources={r"*": {"origins": "*"}})
 api = Api(application)
 
 parser = reqparse.RequestParser()
-parser.add_argument('source', default='stockx')
+parser.add_argument('source', type=str, default='stockx')
+parser.add_argument('search', type=str, default='jordan')
 parser.add_argument('size', type=int)
 parser.add_argument('price_low', type=int, default=0)
 parser.add_argument('price_high', type=int, default=1000)
@@ -41,6 +41,7 @@ class Search(Resource):
 
         args = parser.parse_args()
         source = args['source']
+        search = args['search']
         size = args['size']
         price_low = args['price_low']
         price_high = args['price_high']
@@ -48,12 +49,12 @@ class Search(Resource):
 
         if size is None:
             query = """SELECT * FROM {}_sneakers 
-                       WHERE price >= {} AND price <= {}
-                       LIMIT 1000 OFFSET {};""".format(source, price_low, price_high, offset)
+                       WHERE MATCH(model) AGAINST('{}') AND price >= {} AND price <= {}
+                       LIMIT 1000 OFFSET {};""".format(source, search, price_low, price_high, offset)
         else:
             query = """SELECT * FROM {}_sneakers 
-                       WHERE size={} AND price >= {} AND price <= {}
-                       LIMIT 1000 OFFSET {};""".format(source, size, price_low, price_high, offset)
+                       WHERE MATCH(model) AGAINST('{}') AND size={} AND price >= {} AND price <= {}
+                       LIMIT 1000 OFFSET {};""".format(source, search, size, price_low, price_high, offset)
         
         try:
             c.execute(query)
