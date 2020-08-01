ecc8f3c7458b88f5668a19b0576d40cdb4872853
Avery King <avery.king@mail.utoronto.ca>
2020-07-31 19:29:21 -0500

    fetch to API

--- sanctuary-react/src/components/catalog.js
+++ sanctuary-react/src/components/catalog.js
@@ -1,66 +1,78 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Sneaker from './sneaker'
-import mock_data from '../assets/mock-data'
+
+const API_URL = "http://flask-env.eba-wjhtntpd.us-west-2.elasticbeanstalk.com/?"
 
 
 export default function Catalog(props) {
+    const [data, setData] = useState([])
 
-    function filteredSneakers() {
-        let filteredSneakers = mock_data
-
-        // Filter by model
-        if (props.search) {
-            filteredSneakers = filteredSneakers.filter(
-                (sneaker) => {
-                    return sneaker.model.toLowerCase()
-                    .indexOf(props.search.toLowerCase()) !== -1
-                }
-            )
+    useEffect(() => {
+        const fetchData = async (url) => {
+            const response = await fetch(url)
+            const data = await response.json()
+            setData(data)
         }
+
+        fetchData(API_URL)
+    }, [])
+
+
+    // function filteredSneakers() {
         
-        // Filter by size
-        if (props.sizeFilter) {
-            filteredSneakers = filteredSneakers.filter(
-                (sneaker) => {
-                    return sneaker.size === parseInt(props.sizeFilter)
-                }
-            )
-        }
 
-        // Filter by price
-        if (props.priceFilter) {
-            filteredSneakers = filteredSneakers.filter(
-                (sneaker) => {
-                    return sneaker.price <= parseInt(props.priceFilter)
-                }
-            )
-        }
+    //     // Filter by model
+    //     if (props.search) {
+    //         filteredSneakers = filteredSneakers.filter(
+    //             (sneaker) => {
+    //                 return sneaker.model.toLowerCase()
+    //                 .indexOf(props.search.toLowerCase()) !== -1
+    //             }
+    //         )
+    //     }
+        
+    //     // Filter by size
+    //     if (props.sizeFilter) {
+    //         filteredSneakers = filteredSneakers.filter(
+    //             (sneaker) => {
+    //                 return sneaker.size === parseInt(props.sizeFilter)
+    //             }
+    //         )
+    //     }
 
-        // Filter by site
-        if (props.siteFilter)
-        filteredSneakers = filteredSneakers.filter(
-            (sneaker) => {
-                return sneaker.source_site.toLowerCase()
-                .indexOf(props.siteFilter.toLowerCase()) !== -1
-            }
-        )
+    //     // Filter by price
+    //     if (props.priceFilter) {
+    //         filteredSneakers = filteredSneakers.filter(
+    //             (sneaker) => {
+    //                 return sneaker.price <= parseInt(props.priceFilter)
+    //             }
+    //         )
+    //     }
 
-        return filteredSneakers
-    }
+    //     // Filter by site
+    //     if (props.siteFilter)
+    //     filteredSneakers = filteredSneakers.filter(
+    //         (sneaker) => {
+    //             return sneaker.source_site.toLowerCase()
+    //             .indexOf(props.siteFilter.toLowerCase()) !== -1
+    //         }
+    //     )
+
+    //     return filteredSneakers
+    // }
 
     return (
         <div className='catalog'>
-                {filteredSneakers().map((sneaker) => {
+                {data.map((sneaker) => {
                      return (
                      <Sneaker
                         key={sneaker.id}
-                        brand={sneaker.brand}
                         size={sneaker.size}
                         price={sneaker.price}
                         url={sneaker.url}
                         model={sneaker.model}
-                        source_site={sneaker.source_site}
-                        img={sneaker.img}
+                        source={sneaker.source.toLowerCase()}
+                        image={sneaker.image}
                     />
                     )})}
             </div>
