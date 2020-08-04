55282820c9201352460d68a3f60083ad91dac660
Jason Thomo <amjbvic@gmail.com>
2020-08-04 15:39:44 -0700

    Modifications to stockx attributes

--- Stockx/stockx_api_scraper.py
+++ Stockx/stockx_api_scraper.py
@@ -1,5 +1,4 @@
 import requests
-import api_auth
 import time
 import mysql.connector
 from mysql.connector import ProgrammingError
@@ -22,24 +21,24 @@ def create_db_table():
     """ Creates an empty database table with the necessary keys."""
     try:
         c.execute("""CREATE TABLE stockx_sneakers (
-            id VARCHAR(200) primary key,
+            id INT PRIMARY KEY,
             source VARCHAR(50),
             model TEXT,
             sku_id VARCHAR(20),
             size FLOAT,
-            category TEXT,
-            retail_price INT,
             price INT,
+            shoe_condition VARCHAR(30),
+            category TEXT,
+            recently_sold INT,
+            annual_sold INT,
+            retail_price INT,
             highest_bid INT,
             annual_high_price INT,
             annual_low_price INT,
             average_price INT,
-            average_price_rank INT,
             volatility FLOAT, 
             number_of_asks INT,
             number_of_bids INT,
-            annual_sold INT,
-            recently_sold INT,
             url TEXT,
             image TEXT
         )""")
@@ -60,7 +59,13 @@ def get_api_data(start_size, end_size):
     """
 
     url = "https://stockx.com/api/browse"
-    headers = api_auth.headers
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+        "referer": "https://stockx.com/retro-jordans",
+        "accept": "*/*",
+        "appos": "web",
+        "appversion": "0.1"
+    }
 
     items = []
     size = start_size
@@ -84,19 +89,19 @@ def get_api_data(start_size, end_size):
                 if item["market"]["lowestAsk"] == 0 or item["market"]["lowestAsk"] == None:
                     continue
                 item_data = {
-                    "id": item["objectID"],
+                    "id": abs(hash(item["objectID"]) % (10**7)),
                     "source": "StockX",
                     "model": item["title"],
                     "skuId": item["styleId"],
                     "size": item["shoeSize"],
+                    "condition": item["condition"],
                     "category": item["category"],
+                    "price": item["market"]["lowestAsk"],
                     "retailPrice": item["retailPrice"],
-                    "lowestAsk": item["market"]["lowestAsk"],
                     "highestBid": item["market"]["highestBid"],
                     "annualHigh": item["market"]["annualHigh"],
                     "annualLow": item["market"]["annualLow"],
                     "averagePrice": item["market"]["averageDeadstockPrice"],
-                    "averagePriceRank": item["market"]["averageDeadstockPriceRank"],
                     "volatility": item["market"]["volatility"],
                     "numberOfAsks": item["market"]["numberOfAsks"],
                     "numberOfBids": item["market"]["numberOfBids"],
@@ -132,18 +137,17 @@ def insert_items(item_data):
 
     data_list = []
     for item in item_data:
-        data_list.append((item["id"],item["source"],item["model"],item["skuId"],item["size"],
-            item["category"],item["retailPrice"],item["lowestAsk"],item["highestBid"],item["annualHigh"],
-            item["annualLow"],item["averagePrice"], item["averagePriceRank"],item["volatility"],
-            item["numberOfAsks"],item["numberOfBids"], item["annualSold"], item["recentSold"],
-            item["url"], item["image"]))
+        data_list.append((item["id"],item["source"],item["model"],item["skuId"],item["size"],item["price"],
+            item["condition"],item["category"],item["recentSold"],item["annualSold"],item["retailPrice"],
+            item["highestBid"],item["annualHigh"],item["annualLow"],item["averagePrice"],item["volatility"], 
+            item["numberOfAsks"], item["numberOfBids"], item["url"], item["image"]))
     
     try:
         c.executemany("""INSERT IGNORE INTO stockx_sneakers 
-            (id,source,model,sku_id,size,category,retail_price,
-            price,highest_bid,annual_high_price,annual_low_price,
-            average_price,average_price_rank,volatility,number_of_asks,
-            number_of_bids,annual_sold,recently_sold,url,image) 
+            (id,source,model,sku_id,size,price,shoe_condition,category,
+            recently_sold,annual_sold,retail_price,highest_bid,
+            annual_high_price,annual_low_price,average_price,
+            volatility,number_of_asks,number_of_bids,url,image) 
             VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""", data_list)
         conn.commit()
     except ProgrammingError:
@@ -166,10 +170,10 @@ def run_scraper(start_size, end_size):
     insert_items(data)
 
 
-#create_db_table()
+create_db_table()
 
-run_scraper(6, 9)        # Range 1
-#run_scraper(9.5, 12.5)   # Range 2
+#run_scraper(6, 9)        # Range 1
+run_scraper(9.5, 12.5)   # Range 2
 #run_scraper(13, 15)      # Range 3
 
 
