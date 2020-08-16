e2fa04d2bb46bf1e12de6a95848a8fc4399c0225
raineakiyama-chen <#bacongriffin1bjj>
2020-08-16 03:17:34 -0400

    Styled shoe model pop up

--- sanctuary-react/src/components/Pages/shoemodal.js
+++ sanctuary-react/src/components/Pages/shoemodal.js
@@ -14,45 +14,59 @@ export default function ShoeModal() {
     // TODO: have grailed condition display as regular text
     return (
     <div className="modal">
-        <div className="shoe-grid" ref={wrapperRef}>
+        <div className="dashboard" ref={wrapperRef}>
 
-            <div className="shoe-model">
-                <h2>{shoe.model}</h2>
+            <div className="left"> 
+
+                <div className="shoe-model">
+                    <h2>{shoe.model}</h2>
+                </div>
+
+                <div className="shoe-size">
+                    <h3>Size</h3>
+                    <span>{shoe.size}</span>
+                </div>
+
+                <div className="shoe-condition">
+                    <h3>Condition</h3>
+                    <span>{shoe.shoe_condition}</span>
+                </div>
+
             </div>
 
-            <div className="shoe-image">
-                <img 
-                    src={shoe.image}
-                    alt={shoe.model}
-                />
-            </div>
+            <div className="middle">
 
-            <div className="shoe-price">
-                <h3>Price</h3>
-                <span>{shoe.price}</span>
-            </div>
-
-            <div className="shoe-size">
-                <h3>Size</h3>
-                <span>{shoe.size}</span>
-            </div>
-
-            <div className="shoe-condition">
-                <h3>Condition</h3>
-                <span>{shoe.shoe_condition}</span>
-            </div>
-
-            <div className="shoe-source">
-                {shoe.source &&
+                <div className="shoe-image">
                     <img 
-                        src={require(`../../assets/images/logos/${shoe.source.toLowerCase()}.png`)}
-                        alt={shoe.source}
+                        src={shoe.image}
+                        alt={shoe.model}
                     />
-                }
+                </div>
+
             </div>
 
-            <div className="shoe-buy"> 
-                <a target="_blank " href={`https://${url}`}>Buy Now</a>
+            <div className="right">
+
+                <div className="shoe-price">
+                    <h3>Price</h3>
+                    <span>{shoe.price}</span>
+                </div>
+
+                <div className="shoe-source">
+                    {shoe.source &&
+                        <img 
+                            src={require(`../../assets/images/logos/${shoe.source.toLowerCase()}.png`)}
+                            alt={shoe.source}
+                        />
+                    }
+                </div>
+
+                <div className="shoe-buy">
+                    <span>
+                        <a target="_blank " href={`https://${url}`}>Buy Now</a>
+                    </span>
+                </div>
+
             </div>
 
         </div>
