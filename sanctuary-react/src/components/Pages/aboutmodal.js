8f07b248d7ef6a7665be3fbf8bdb34efdcb2478a
Avery King <avery.king@mail.utoronto.ca>
2020-08-23 19:48:21 -0500

    close buttons on mobile modals

--- sanctuary-react/src/components/Pages/aboutmodal.js
+++ sanctuary-react/src/components/Pages/aboutmodal.js
@@ -1,5 +1,8 @@
 import React, { useRef } from 'react'
 import useOutsideAlerter from '../useoutsidealerter'
+import { RiCloseLine } from 'react-icons/ri'
+import { useDispatch } from 'react-redux'
+import { hideAboutModal } from '../../redux/actions'
 
 import picture1 from '../../assets/images/aboutPic1.jpg'
 import picture2 from '../../assets/images/aboutPic2.jpg'
@@ -9,97 +12,104 @@ import picture3 from '../../assets/images/aboutPic3.jpg'
 export default function AboutModal() {
 
     const wrapperRef = useRef(null)
+    const dispatch = useDispatch()
     useOutsideAlerter(wrapperRef)
 
-    // TODO: add an escape button for when there is no background, to close window
-
     return (
-    <div className="modal">
-        <div className="about" ref={wrapperRef}>
+        <div className="modal">
 
-            <div className='cardOne'>
-                <h1>If you love sneakers,</h1>
-                <h2>You've come to the right place</h2>
-                <img src={picture1} alt="shoes"/>
-                <p>
-                    At Sanctuary, we aim to provide you with the best possible 
-                    deals for the streetwear items you love. Our mission is not 
-                    only to act as an online marketplace but also a central 
-                    location for streetwear market data.
-                </p>
-            </div>
+            <div className="about" ref={wrapperRef}>
 
-            <div className='cardTwo'>
-                <h1>More items, brands, and features soon!</h1>
-                <p>
-                    Sanctuary was founded by five like-minded individuals with 
-                    aspirations to revolutionize the fashion industry. As 
-                    individuals, we strive to create a global hub for streetwear 
-                    market data through our core values of innovation, 
-                    creativity, originality, and integrity.
-                </p>
-                <p>
-                    Our services are currently limited to Air Jordans, however, 
-                    our team is continuously working on updating our database 
-                    to provide you with a wide range of streetwear products.
-                </p>
-                <img src={picture2} alt="shoes"/>
-            </div>
-
-            <div className='cardThree'>
-                <h1>Our Services:</h1>
-                <h2>How it works</h2>
-                
-                <div className='bottom'>
-                    <div className='leftText'>
-                        <h3>Specific site?</h3>
-                        <p>
-                            Browse each website by row 
-                            right on the homepage.
-                        </p>
-
-                        <h3>Specific filters?</h3>
-                        <p>
-                            Customize filters such as size 
-                            and price to find the exact 
-                            model of sneaker you desire.
-                        </p>
-
-                        <h3>Specific items?</h3>
-                        <p>
-                            Search for a sneaker you love, 
-                            and we will compare prices from 
-                            FlightClub, Goat, Grailed, and StockX 
-                            to provide with the best price.
-                        </p>
-
-                        <h3>Ready to buy?</h3>
-                        <p>
-                            Simply click the “Buy Now” button 
-                            and you’ll be redirected to the 
-                            website that is providing your 
-                            item for its desired price!
-                        </p>
-                    </div>
-                    <img src={picture3} alt="shoes"/>
+                <div
+                    className='closeButton'
+                    onClick={() => dispatch(hideAboutModal())}
+                >
+                    <RiCloseLine />
                 </div>
-            </div>
 
-            <div className='cardFour'>
-                <div className='fourWrap'>
-                    <h1>We love feedback!</h1>
-                    <h2>
-                        Is there something you think we could do
-                        better? Please feel free to contact us and 
-                        we will get back to you shortly!
+                <div className='cardOne'>
+                    <h1>If you love sneakers,</h1>
+                    <h2>You've come to the right place</h2>
+                    <img src={picture1} alt="shoes" />
+                    <p>
+                        At Sanctuary, we aim to provide you with the best possible
+                        deals for the streetwear items you love. Our mission is not
+                        only to act as an online marketplace but also a central
+                        location for streetwear market data.
+                </p>
+                </div>
+
+                <div className='cardTwo'>
+                    <h1>More items, brands, and features soon!</h1>
+                    <p>
+                        Sanctuary was founded by five like-minded individuals with
+                        aspirations to revolutionize the fashion industry. As
+                        individuals, we strive to create a global hub for streetwear
+                        market data through our core values of innovation,
+                        creativity, originality, and integrity.
+                </p>
+                    <p>
+                        Our services are currently limited to Air Jordans, however,
+                        our team is continuously working on updating our database
+                        to provide you with a wide range of streetwear products.
+                </p>
+                    <img src={picture2} alt="shoes" />
+                </div>
+
+                <div className='cardThree'>
+                    <h1>Our Services:</h1>
+                    <h2>How it works</h2>
+
+                    <div className='bottom'>
+                        <div className='leftText'>
+                            <h3>Specific site?</h3>
+                            <p>
+                                Browse each website by row
+                                right on the homepage.
+                        </p>
+
+                            <h3>Specific filters?</h3>
+                            <p>
+                                Customize filters such as size
+                                and price to find the exact
+                                model of sneaker you desire.
+                        </p>
+
+                            <h3>Specific items?</h3>
+                            <p>
+                                Search for a sneaker you love,
+                                and we will compare prices from
+                                FlightClub, Goat, Grailed, and StockX
+                                to provide with the best price.
+                        </p>
+
+                            <h3>Ready to buy?</h3>
+                            <p>
+                                Simply click the “Buy Now” button
+                                and you’ll be redirected to the
+                                website that is providing your
+                                item for its desired price!
+                        </p>
+                        </div>
+                        <img src={picture3} alt="shoes" />
+                    </div>
+                </div>
+
+                <div className='cardFour'>
+                    <div className='fourWrap'>
+                        <h1>We love feedback!</h1>
+                        <h2>
+                            Is there something you think we could do
+                            better? Please feel free to contact us and
+                            we will get back to you shortly!
                     </h2>
 
-                    <div className='box'>
-                        <button> sanctuary@gmail.com </button>
+                        <div className='box'>
+                            <button> sanctuary@gmail.com </button>
+                        </div>
                     </div>
                 </div>
-            </div> 
+            </div>
         </div>
-    </div>
     )
 }
--- sanctuary-react/src/components/Pages/shoemodal.js
+++ sanctuary-react/src/components/Pages/shoemodal.js
@@ -1,10 +1,14 @@
 import React, { useRef } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import useOutsideAlerter from '../useoutsidealerter'
+import { hideShoeModal } from '../../redux/actions'
+import { RiCloseLine } from 'react-icons/ri'
 
 
 export default function ShoeModal() {
+
     const shoe = useSelector(state => state.shoe)
+    const dispatch = useDispatch()
     const wrapperRef = useRef(null)
     useOutsideAlerter(wrapperRef)
 
@@ -13,144 +17,151 @@ export default function ShoeModal() {
     url = url.replace(/(^\w+:|^)\/\//, '')
 
     return (
-    <div className="modal">
-        <div className="dashboard" ref={wrapperRef}>
+        <div className="modal">
 
-            <div className="web-version">
-
-                <div className="left"> 
-                    <div className='shoe-model-container'>
-                        <div className="shoe-model">
-                            <h2>{shoe.model}</h2>
-                        </div>
-                    </div>
-
-                    <div className='size-and-condition'>
-                        <div className='shoe-size-container'>
-                            <div className="shoe-size">
-                                <h3>Size</h3>
-                                <span>{shoe.size}</span>
-                            </div>
-                        </div>
-
-                        <div className='shoe-condition-container'>
-                            <div className="shoe-condition">
-                                <h3>Condition</h3>
-                                <span>{shoe.shoe_condition}</span>
-                            </div>
-                        </div>
-                    </div>
+            <div className="dashboard" ref={wrapperRef}>
+                <div
+                    className='closeButton'
+                    onClick={() => dispatch(hideShoeModal())}
+                >
+                    <RiCloseLine />
                 </div>
 
-                <div className="middle">
+                <div className="web-version">
+
+                    <div className="left">
+                        <div className='shoe-model-container'>
+                            <div className="shoe-model">
+                                <h2>{shoe.model}</h2>
+                            </div>
+                        </div>
+
+                        <div className='size-and-condition'>
+                            <div className='shoe-size-container'>
+                                <div className="shoe-size">
+                                    <h3>Size</h3>
+                                    <span>{shoe.size}</span>
+                                </div>
+                            </div>
+
+                            <div className='shoe-condition-container'>
+                                <div className="shoe-condition">
+                                    <h3>Condition</h3>
+                                    <span>{shoe.shoe_condition}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="middle">
                         <div className="shoe-image">
-                            <img 
+                            <img
                                 src={shoe.image}
                                 alt={shoe.model}
                             />
                         </div>
+                    </div>
+
+                    <div className="right">
+                        <div className="shoe-price-container">
+                            <div className="shoe-price">
+                                <h3>Price</h3>
+                                <span>${shoe.price}</span>
+                            </div>
+                        </div>
+
+                        <div className="shoe-source-container">
+                            <div className="shoe-source">
+                                {shoe.source &&
+                                    <img
+                                        src={require(`../../assets/images/logos/${shoe.source.toLowerCase()}.png`)}
+                                        alt={shoe.source}
+                                    />
+                                }
+                            </div>
+                        </div>
+
+                        <div className="shoe-buy-container">
+                            <div className="shoe-buy">
+                                <span>
+                                    <a target="_blank " href={`https://${url}`}>Buy Now</a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="right">
-                    <div className="shoe-price-container">
-                        <div className="shoe-price">
-                            <h3>Price</h3>
-                            <span>${shoe.price}</span>
+
+                {/* MOBILE VERSION  */}
+
+
+                <div className="mobile-version">
+
+                    <div className='m-top'>
+                        <div className="m-shoe-model">
+                            <h2>{shoe.model}</h2>
                         </div>
                     </div>
 
-                    <div className="shoe-source-container">
-                        <div className="shoe-source">
-                            {shoe.source &&
-                                <img 
-                                    src={require(`../../assets/images/logos/${shoe.source.toLowerCase()}.png`)}
-                                    alt={shoe.source}
-                                />
-                            }
+                    <div className="m-middle">
+                        <div className="m-middle-left">
+
+                            <div className="m-shoe-image-container">
+                                <div className="m-shoe-image">
+                                    <img
+                                        src={shoe.image}
+                                        alt={shoe.model}
+                                    />
+                                </div>
+                            </div>
+
+
+                            <div className="m-shoe-source">
+                                {shoe.source &&
+                                    <img
+                                        src={require(`../../assets/images/logos/${shoe.source.toLowerCase()}.png`)}
+                                        alt={shoe.source}
+                                    />
+                                }
+                            </div>
+                        </div>
+
+                        <div className="m-middle-right">
+
+                            <div className='m-shoe-price-container'>
+                                <div className="m-shoe-price">
+                                    <h3>Price</h3>
+                                    <span>${shoe.price}</span>
+                                </div>
+                            </div>
+
+                            <div className="m-shoe-size-container">
+                                <div className="m-shoe-size">
+                                    <h3>Size</h3>
+                                    <span>{shoe.size}</span>
+                                </div>
+                            </div>
+
+                            <div className="m-shoe-condition-container">
+                                <div className="m-shoe-condition">
+                                    <h3>Condition</h3>
+                                    <span>{shoe.shoe_condition}</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="shoe-buy-container">
-                        <div className="shoe-buy">
+                    <div className="m-bottom">
+
+                        <div className="m-shoe-buy">
                             <span>
                                 <a target="_blank " href={`https://${url}`}>Buy Now</a>
                             </span>
                         </div>
                     </div>
                 </div>
+
             </div>
-
-
-            {/* MOBILE VERSION  */}
-
-
-            <div className="mobile-version">
-
-                <div className='m-top'>
-                    <div className="m-shoe-model">
-                        <h2>{shoe.model}</h2>
-                    </div>
-                </div>
-
-                <div className="m-middle">
-                    <div className="m-middle-left">
-
-                        <div className="m-shoe-image-container">
-                            <div className="m-shoe-image">
-                                <img 
-                                    src={shoe.image}
-                                    alt={shoe.model}
-                                />
-                            </div>
-                        </div>
-
-                        
-                        <div className="m-shoe-source">
-                            {shoe.source &&
-                                <img 
-                                    src={require(`../../assets/images/logos/${shoe.source.toLowerCase()}.png`)}
-                                    alt={shoe.source}
-                                />
-                            }
-                        </div>
-                    </div>
-
-                    <div className="m-middle-right">
-                        
-                        <div className='m-shoe-price-container'>
-                            <div className="m-shoe-price">
-                                <h3>Price</h3>
-                                <span>${shoe.price}</span>
-                            </div>
-                        </div>
-
-                        <div className="m-shoe-size-container">
-                            <div className="m-shoe-size">
-                                <h3>Size</h3>
-                                <span>{shoe.size}</span>
-                            </div>
-                        </div>
-
-                        <div className="m-shoe-condition-container">
-                            <div className="m-shoe-condition">
-                                <h3>Condition</h3>
-                                <span>{shoe.shoe_condition}</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="m-bottom">
-
-                    <div className="m-shoe-buy">
-                        <span>
-                            <a target="_blank " href={`https://${url}`}>Buy Now</a>
-                        </span>
-                    </div>
-                </div>
-            </div>
-
         </div>
-    </div>
     )
 }
