2e33c9d88668b301f58a52ba35ace0641556738f
Avery King <avery.king@mail.utoronto.ca>
2020-09-04 02:25:23 -0500

    div100vh fix

--- sanctuary-react/src/components/Pages/home.js
+++ sanctuary-react/src/components/Pages/home.js
@@ -3,16 +3,22 @@ import { showAboutModal } from '../../redux/actions'
 import { useDispatch } from 'react-redux'
 import Catalog from '../catalog'
 import FilterBar from '../filterbar'
-import Div100vh from 'react-div-100vh'
+import { use100vh } from 'react-div-100vh'
+import { useMediaQuery } from 'react-responsive'
 
 
 export default function Home() {
 
     const dispatch = useDispatch()
+    const isDesktop = useMediaQuery({
+        query: '(min-width: 930px)'
+    })
+    const height = use100vh()
+    const recalculatedHeight = isDesktop ? height - 91 : height - 61 
 
     return (
         <React.Fragment>
-            <Div100vh className="splash">
+            <div className="splash" style={{ height: recalculatedHeight }}>
                 <div className='bg-image'></div>
                 <div className='splashWrap'>
                     <h1>Find Your Perfect Pair</h1>
@@ -26,7 +32,7 @@ export default function Home() {
                     </button>
                     </div>
                 </div>
-            </Div100vh>
+            </div>
 
             <main className='filter-catalog'>
                 <section id="section-a" className='filterrow'>
