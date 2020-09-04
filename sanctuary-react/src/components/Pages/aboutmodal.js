d6e6fd12723e0d341559f6e585aba57017d2dc7e
Avery King <avery.king@mail.utoronto.ca>
2020-09-04 02:05:51 -0500

    fixed div 100 issue for mobile

--- sanctuary-react/src/components/Pages/aboutmodal.js
+++ sanctuary-react/src/components/Pages/aboutmodal.js
@@ -3,6 +3,7 @@ import useOutsideAlerter from '../useoutsidealerter'
 import { RiCloseLine } from 'react-icons/ri'
 import { useDispatch } from 'react-redux'
 import { hideAboutModal } from '../../redux/actions'
+import Div100vh from 'react-div-100vh'
 
 import picture1 from '../../assets/images/aboutPic1.jpg'
 import picture2 from '../../assets/images/aboutPic2.jpg'
@@ -17,8 +18,8 @@ export default function AboutModal() {
 
     useEffect(() => {
         document.body.style.overflow = 'hidden';
-        return ()=> document.body.style.overflow = 'unset';
-     }, []);
+        return () => document.body.style.overflow = 'unset';
+    }, []);
 
     return (
         <div className="modal-about">
@@ -27,7 +28,7 @@ export default function AboutModal() {
 
                 <div className='closeButton'
                     onClick={() => dispatch(hideAboutModal())}>
-                    <RiCloseLine/>
+                    <RiCloseLine />
                 </div>
 
                 <div className='cardOne'>
@@ -35,10 +36,10 @@ export default function AboutModal() {
                     <h2>You've come to the right place</h2>
                     <img src={picture1} alt="shoes" />
                     <p>
-                        Sanctuary aims to provide the best possible deals on any 
-                        streetwear or sneakers you could want. Our mission is to 
-                        evolve the paradigm of fashion by leveraging big data, 
-                        analytics, and artificial intelligence to provide one 
+                        Sanctuary aims to provide the best possible deals on any
+                        streetwear or sneakers you could want. Our mission is to
+                        evolve the paradigm of fashion by leveraging big data,
+                        analytics, and artificial intelligence to provide one
                         centralized online location for finding your perfect piece.
                 </p>
                 </div>
@@ -51,29 +52,29 @@ export default function AboutModal() {
                         <div className='leftText'>
                             <h3>Specific site?</h3>
                             <p>
-                                On the homepage, browse websites 
+                                On the homepage, browse websites
                                 by row.
                         </p>
 
                             <h3>Specific filters?</h3>
                             <p>
-                                Customize filters such as size, 
+                                Customize filters such as size,
                                 condition, and price.
                         </p>
 
                             <h3>Specific item?</h3>
                             <p>
-                                Search for a sneaker, and we will 
-                                provide you with the best price. We 
-                                compare prices from StockX, Goat, 
+                                Search for a sneaker, and we will
+                                provide you with the best price. We
+                                compare prices from StockX, Goat,
                                 Grailed, and FlightClub.
                         </p>
 
                             <h3>Ready to buy?</h3>
                             <p>
-                                Found an item you wish to purchase? 
-                                Simply click the “buy now” button and 
-                                you will be redirected to the seller’s 
+                                Found an item you wish to purchase?
+                                Simply click the “buy now” button and
+                                you will be redirected to the seller’s
                                 website.
                         </p>
                         </div>
@@ -84,23 +85,23 @@ export default function AboutModal() {
                 <div className='cardTwo'>
                     <h1>More items, brands, and features soon!</h1>
                     <p>
-                        The Sanctuary team is built on a set of core values; namely, 
-                        innovation, creativity, and integrity. However, creating the 
-                        best user experience, and ultimately helping our users as much 
+                        The Sanctuary team is built on a set of core values; namely,
+                        innovation, creativity, and integrity. However, creating the
+                        best user experience, and ultimately helping our users as much
                         as possible is our overarching focus.
                     </p>
                     <p>
-                        You have found Sanctuary at a very early stage. We are currently 
-                        in early Alpha and have just begun our mission. Although our current 
-                        stock is limited, and some functionality may be crude, we will be 
-                        constantly updating, expanding, and improving the site. We have a team 
-                        dedicated to making an amazing product. Any feedback or suggestions 
+                        You have found Sanctuary at a very early stage. We are currently
+                        in early Alpha and have just begun our mission. Although our current
+                        stock is limited, and some functionality may be crude, we will be
+                        constantly updating, expanding, and improving the site. We have a team
+                        dedicated to making an amazing product. Any feedback or suggestions
                         are welcome (see our contact page)
                     </p>
                     <img src={picture2} alt="shoes" />
                 </div>
 
-                <div className='cardFour'>
+                <Div100vh className='cardFour'>
                     <div className='fourWrap'>
                         <h1>We love feedback!</h1>
                         <h2>
@@ -113,7 +114,7 @@ export default function AboutModal() {
                             <button> contact@sanctuarysneakers.com </button>
                         </div>
                     </div>
-                </div>
+                </Div100vh>
             </div>
         </div>
     )
--- sanctuary-react/src/components/Pages/home.js
+++ sanctuary-react/src/components/Pages/home.js
@@ -3,43 +3,44 @@ import { showAboutModal } from '../../redux/actions'
 import { useDispatch } from 'react-redux'
 import Catalog from '../catalog'
 import FilterBar from '../filterbar'
+import Div100vh from 'react-div-100vh'
 
 
 export default function Home() {
 
     const dispatch = useDispatch()
 
-    return (    
-    <React.Fragment>
-        <div className="splash">
-            <div className='bg-image'></div>
-            <div className='splashWrap'>
-                <h1>Find Your Perfect Pair</h1>
-                <h3>Get the best price on Jordans from your favourite websites</h3>
-                <div className='twoButtons'>
-                    <a href="#section-a" className="shopNowBtn"> 
-                        Shop Now
+    return (
+        <React.Fragment>
+            <Div100vh className="splash">
+                <div className='bg-image'></div>
+                <div className='splashWrap'>
+                    <h1>Find Your Perfect Pair</h1>
+                    <h3>Get the best price on Jordans from your favourite websites</h3>
+                    <div className='twoButtons'>
+                        <a href="#section-a" className="shopNowBtn">
+                            Shop Now
                     </a>
-                    <button className='aboutUsBtn' onClick={() => dispatch(showAboutModal())}>
-                        About Us
+                        <button className='aboutUsBtn' onClick={() => dispatch(showAboutModal())}>
+                            About Us
                     </button>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </Div100vh>
 
-        <main className='filter-catalog'>
-            <section id="section-a" className='filterrow'>
-                <div className='a-wrap'>
-                    <FilterBar/>
-                </div>
-            </section>
+            <main className='filter-catalog'>
+                <section id="section-a" className='filterrow'>
+                    <div className='a-wrap'>
+                        <FilterBar />
+                    </div>
+                </section>
 
-            <section id="section-b" className='catalogrow'>
-                <div className='b-wrap'>
-                <Catalog/>
-                </div>
-            </section>
-        </main>
-    </React.Fragment>
+                <section id="section-b" className='catalogrow'>
+                    <div className='b-wrap'>
+                        <Catalog />
+                    </div>
+                </section>
+            </main>
+        </React.Fragment>
     )
 }
