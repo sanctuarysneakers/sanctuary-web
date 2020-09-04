b0c601cce169c69cc21fb858cc78e8784a4a8e90
raineakiyama-chen <#bacongriffin1bjj>
2020-09-04 03:43:12 -0400

    styled pagenotfound page

--- sanctuary-react/src/components/Pages/pagenotfound.js
+++ sanctuary-react/src/components/Pages/pagenotfound.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function PageNotFound() {
-    return <h1>Page Not Found :(</h1>
+    return (
+        <div className="pageNotFoundText">
+            <h1>Sorry, we can't find the page you're looking for.</h1>
+        </div>
+    )
 }
