1df7820323d50db82141c078d3bb3474dd8594a4
EricHasegawa <eric.hasegawa@mail.utoronto.ca>
2020-10-25 10:37:24 -0400

    page now refreshes when logo is clicked

--- sanctuary-react/src/components/navbar.js
+++ sanctuary-react/src/components/navbar.js
@@ -7,18 +7,22 @@ import sanctuaryLogo from "../assets/images/sanctuary-logo-row.png"
 
 export default function NavBar() {
 
-    const isCollapsed = useSelector(state => state.isSearchBarCollapsed)
+    const isCollapsed = useSelector(state => state.isSearchBarCollapsed);
 
     return (
         <nav className='navbar'>
             <div className='navbarContent'>
                 {isCollapsed &&
                     <Link to={"/"}>
+                        <a href="#" onClick={() => window.location.reload()}>
+                        <a href="#" onClick={() => window.scrollTo(0, 0)}>
                         <img
                             className='sanctuaryLogo'
                             src={sanctuaryLogo}
                             alt={"Sanctuary"}
                         />
+                        </a>
+                        </a>
                     </Link>}
                 <SearchBar />
             </div>
