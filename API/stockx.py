ada92fea51956918543898a23efb9ed1c1ae9a69
Jason Thomo <amjbvic@gmail.com>
2020-12-10 21:11:57 -0800

    added more headers to stockx api call, setup EB with git repo, formatting changes

--- API/stockx.py
+++ API/stockx.py
@@ -3,43 +3,47 @@ from urllib.parse import urlencode
 
 
 def get_data(search_query, shoe_size, price_low, price_high, page, page_len):
-    url = "https://stockx.com/api/browse"
-    headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
-        "referer": "https://stockx.com",
-        "accept": "*/*",
-        "appos": "web",
-        "appversion": "0.1"
-    }
+	url = "https://stockx.com/api/browse"
+	headers = {
+		"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"referer": "https://stockx.com",
+		"accept": "*/*",
+		"accept-encoding": "gzip, deflate, br",
+		"accept-language": "en-US,en;q=0.9,sq;q=0.8,it;q=0.7",
+		"appos": "web",
+		"appversion": "0.1",
+		"x-requested-with": "XMLHttpRequest",
+		"cache-control": "no-cache"
+	}
 
-    parameters = {
-        "productCategory": "sneakers",
-        "_search": search_query,
-        "gender": "men",
-        "shoeSize": shoe_size,
-        "market.lowestAsk": f"range({price_low}|{price_high})",
-        "page": page
-    }
-    response = requests.get(url, headers=headers, params=parameters)
-    response.raise_for_status()
-    request_data = response.json()["Products"]
+	parameters = {
+		"productCategory": "sneakers",
+		"_search": search_query,
+		"gender": "men",
+		"shoeSize": shoe_size,
+		"market.lowestAsk": f"range({price_low}|{price_high})",
+		"page": page
+	}
+	response = requests.get(url, headers=headers, params=parameters)
+	response.raise_for_status()
+	request_data = response.json()["Products"]
 
-    results = []
-    for item in request_data:
-        if len(results) >= page_len:
-            break
-        results.append({
-            "id": abs(hash(item["objectID"]) % (10**7)),
-            "source": "StockX",
-            "model": item["title"],
-            "sku_id": item["styleId"],
-            "size": float(shoe_size),
-            "price": item["market"]["lowestAsk"],
-            "shoe_condition": item["condition"],
-            "category": item["category"],
-            "url": "stockx.com/" + item["urlKey"],
-            "image": item["media"]["imageUrl"],
-            "image_thumbnail": item["media"]["imageUrl"].split('?', 1)[0] + "?w=300&q=50&trim=color"
-        })
+	results = []
+	for item in request_data:
+		if len(results) >= page_len:
+			break
+		results.append({
+			"id": abs(hash(item["objectID"]) % (10**7)),
+			"source": "StockX",
+			"model": item["title"],
+			"sku_id": item["styleId"],
+			"size": float(shoe_size),
+			"price": item["market"]["lowestAsk"],
+			"shoe_condition": item["condition"],
+			"category": item["category"],
+			"url": "stockx.com/" + item["urlKey"],
+			"image": item["media"]["imageUrl"],
+			"image_thumbnail": item["media"]["imageUrl"].split('?', 1)[0] + "?w=300&q=50&trim=color"
+		})
 
-    return results
+	return results
--- Goat/goat_api_scraper.py
+++ Goat/goat_api_scraper.py
@@ -10,143 +10,143 @@ user = "admin"
 passwd = "4tDqfnvbQ8R8RGuh"
 db_name = "sneakers"
 try:
-    conn = mysql.connector.connect(host=host, user=user, passwd=passwd, database=db_name)
-    c = conn.cursor()
-    c.execute("USE sneakers;")
+	conn = mysql.connector.connect(host=host, user=user, passwd=passwd, database=db_name)
+	c = conn.cursor()
+	c.execute("USE sneakers;")
 except ProgrammingError:
-    print("couldn't connect to database")
+	print("couldn't connect to database")
 
 
 def create_db_table():
-    """ Creates an empty database table with the necessary keys."""
-    c.execute("DROP TABLE IF EXISTS goat_sneakers_tmp;")
-    try:
-        c.execute("""CREATE TABLE goat_sneakers_tmp (
-            id INT PRIMARY KEY,
-            source VARCHAR(50),
-            model TEXT,
-            sku_id VARCHAR(20),
-            size FLOAT,
-            price INT,
-            shoe_condition VARCHAR(30),
-            category TEXT,
-            trending BOOLEAN,
-            just_dropped BOOLEAN,
-            url TEXT,
-            image TEXT,
-            image_thumbnail TEXT
-        );""")
-        conn.commit()
-    except ProgrammingError:
-        pass
+	""" Creates an empty database table with the necessary keys."""
+	c.execute("DROP TABLE IF EXISTS goat_sneakers_tmp;")
+	try:
+		c.execute("""CREATE TABLE goat_sneakers_tmp (
+			id INT PRIMARY KEY,
+			source VARCHAR(50),
+			model TEXT,
+			sku_id VARCHAR(20),
+			size FLOAT,
+			price INT,
+			shoe_condition VARCHAR(30),
+			category TEXT,
+			trending BOOLEAN,
+			just_dropped BOOLEAN,
+			url TEXT,
+			image TEXT,
+			image_thumbnail TEXT
+		);""")
+		conn.commit()
+	except ProgrammingError:
+		pass
 
 
 def alter_db_table():
-    c.execute("ALTER TABLE goat_sneakers_tmp ADD INDEX id (id);")
-    c.execute("ALTER TABLE goat_sneakers_tmp ADD FULLTEXT model_idx (model);")
-    
-    c.execute("RENAME TABLE goat_sneakers TO goat_sneakers_old, goat_sneakers_tmp TO goat_sneakers;")
-    c.execute("DROP TABLE goat_sneakers_old;")
-    
-    conn.commit()
+	c.execute("ALTER TABLE goat_sneakers_tmp ADD INDEX id (id);")
+	c.execute("ALTER TABLE goat_sneakers_tmp ADD FULLTEXT model_idx (model);")
+	
+	c.execute("RENAME TABLE goat_sneakers TO goat_sneakers_old, goat_sneakers_tmp TO goat_sneakers;")
+	c.execute("DROP TABLE goat_sneakers_old;")
+	
+	conn.commit()
 
 
 def get_api_data(s_query):
-    """ Returns a very detailed list of items from Goat.
+	""" Returns a very detailed list of items from Goat.
 
-    Arguments:
-        String s_query: The search query (sneaker category) to get data for.
+	Arguments:
+		String s_query: The search query (sneaker category) to get data for.
 
-    Returns:
-        List[Dict] items: A list containing dictionaries, each dictionary contains information on one pair of shoes.
-    """
+	Returns:
+		List[Dict] items: A list containing dictionaries, each dictionary contains information on one pair of shoes.
+	"""
 
-    url = "https://2fwotdvm2o-dsn.algolia.net/1/indexes/product_variants_v2/query"
-    params = {
-        "x-algolia-agent": "Algolia for vanilla JavaScript 3.25.1",
-        "x-algolia-application-id": "2FWOTDVM2O",
-        "x-algolia-api-key": "ac96de6fef0e02bb95d433d8d5c7038a"
-    }
+	url = "https://2fwotdvm2o-dsn.algolia.net/1/indexes/product_variants_v2/query"
+	params = {
+		"x-algolia-agent": "Algolia for vanilla JavaScript 3.25.1",
+		"x-algolia-application-id": "2FWOTDVM2O",
+		"x-algolia-api-key": "ac96de6fef0e02bb95d433d8d5c7038a"
+	}
 
-    items = []
-    sizes = ['6','6.5','7','7.5','8','8.5','9','9.5','10','10.5','11','11.5','12','12.5','13','13.5','14','14.5','15','16']
-    for size in sizes:
-        post_json = {
-            "params": "query=&" + urlencode({
-                "query": s_query,
-                "facetFilters": "(product_category: shoes), (presentation_size:"+size+"), (single_gender: men)",
-                "distinct": "true",
-                "offset": "0",
-                "length": "1000"
-            })
-        }
-        response = requests.post(url, params=params, json=post_json)
-        resp_json = response.json()
+	items = []
+	sizes = ['6','6.5','7','7.5','8','8.5','9','9.5','10','10.5','11','11.5','12','12.5','13','13.5','14','14.5','15','16']
+	for size in sizes:
+		post_json = {
+			"params": "query=&" + urlencode({
+				"query": s_query,
+				"facetFilters": "(product_category: shoes), (presentation_size:"+size+"), (single_gender: men)",
+				"distinct": "true",
+				"offset": "0",
+				"length": "1000"
+			})
+		}
+		response = requests.post(url, params=params, json=post_json)
+		resp_json = response.json()
 
-        hits = resp_json['hits']
-        for hit in hits:
-            item = {
-                'id': int(hit['objectID']),
-                'source': "Goat",
-                'model': hit['name'],
-                'skuId': hit['sku'].replace(' ', '-'),
-                'size': float(size),
-                'category': hit['silhouette'],
-                'shoe_condition': hit['shoe_condition'],
-                'price': hit['lowest_price_cents']/100,
-                'trending': 1 if 'trending' in hit['collection_slugs'] else 0,
-                'just_dropped': 1 if 'just-dropped' in hit['collection_slugs'] else 0,
-                'url': 'https://www.goat.com/sneakers/' + hit['slug'],
-                'image': hit['main_picture_url'],
-                'image_thumbnail': hit['main_picture_url'][:23] + "400" + hit['main_picture_url'][26:]
-            }
-            items.append(item)
+		hits = resp_json['hits']
+		for hit in hits:
+			item = {
+				'id': int(hit['objectID']),
+				'source': "Goat",
+				'model': hit['name'],
+				'skuId': hit['sku'].replace(' ', '-'),
+				'size': float(size),
+				'category': hit['silhouette'],
+				'shoe_condition': hit['shoe_condition'],
+				'price': hit['lowest_price_cents']/100,
+				'trending': 1 if 'trending' in hit['collection_slugs'] else 0,
+				'just_dropped': 1 if 'just-dropped' in hit['collection_slugs'] else 0,
+				'url': 'https://www.goat.com/sneakers/' + hit['slug'],
+				'image': hit['main_picture_url'],
+				'image_thumbnail': hit['main_picture_url'][:23] + "400" + hit['main_picture_url'][26:]
+			}
+			items.append(item)
 
-    return items
+	return items
 
 
 def insert_items(item_data):
-    """ Inserts feed items into db.
+	""" Inserts feed items into db.
 
-    Loops through all items in the feed and inserts them into the db
-    if they don't already exist. If a certain item already exists in the db,
-    it updates information if it has changed.
+	Loops through all items in the feed and inserts them into the db
+	if they don't already exist. If a certain item already exists in the db,
+	it updates information if it has changed.
 
-    Arguments:
-        List[Dict] item_data: A list of dict's containing data for each feed item.
+	Arguments:
+		List[Dict] item_data: A list of dict's containing data for each feed item.
 
-    Returns:
-        No return value.
-    """
+	Returns:
+		No return value.
+	"""
 
-    data_list = []
-    for item in item_data:
-        data_list.append((item["id"], item["source"], item["model"], item['skuId'], item["size"], 
-                          item["price"], item["shoe_condition"], item["category"],item['trending'], 
-                          item['just_dropped'], item["url"], item["image"], item["image_thumbnail"]))
+	data_list = []
+	for item in item_data:
+		data_list.append((item["id"], item["source"], item["model"], item['skuId'], item["size"], 
+						  item["price"], item["shoe_condition"], item["category"],item['trending'], 
+						  item['just_dropped'], item["url"], item["image"], item["image_thumbnail"]))
 
-    try:
-        c.executemany("""INSERT IGNORE INTO goat_sneakers_tmp
-            (id,source,model,sku_id,size,price,shoe_condition,category,trending,just_dropped,url,image,image_thumbnail) 
-            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""", data_list)
-        conn.commit()
-    except ProgrammingError:
-        pass
+	try:
+		c.executemany("""INSERT IGNORE INTO goat_sneakers_tmp
+			(id,source,model,sku_id,size,price,shoe_condition,category,trending,just_dropped,url,image,image_thumbnail) 
+			VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);""", data_list)
+		conn.commit()
+	except ProgrammingError:
+		pass
 
 
 def run_scraper():
-    """ Runs the scraper """
-    
-    categoryList = ["Air Jordan","Nike Running","Air Force","Dunk","Balenciaga","Yeezy","Ultraboost","Nike SB"]
+	""" Runs the scraper """
+	
+	categoryList = ["Air Jordan","Nike Running","Air Force","Dunk","Balenciaga","Yeezy","Ultraboost","Nike SB"]
 
-    # Get a list of all the item data from the api
-    data = []
-    for category in categoryList:
-        data.extend(get_api_data(category))
-    print("Scraped", len(data), "items.")
+	# Get a list of all the item data from the api
+	data = []
+	for category in categoryList:
+		data.extend(get_api_data(category))
+	print("Scraped", len(data), "items.")
 
-    # Insert items into the database
-    insert_items(data)
+	# Insert items into the database
+	insert_items(data)
 
 
 create_db_table()
