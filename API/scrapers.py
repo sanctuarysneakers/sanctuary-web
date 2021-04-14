a28baefbec98dcd147d3e823aaca286ed74e5335
Jason Thomo <amjbvic@gmail.com>
2021-04-13 21:26:44 -0700

    created new scrapers for getting model-specific data

--- API/scrapers.py
+++ API/scrapers.py
@@ -48,6 +48,37 @@ def get_stockx_data(search_query, shoe_size, price_low, price_high, page, page_l
 
 	return results
 
+def get_stockx_item_data(sku_id, size, price_low, price_high):
+    url = "https://stockx.com/api/browse"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+        "referer": "https://stockx.com",
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9,sq;q=0.8,it;q=0.7",
+        "appos": "web",
+        "appversion": "0.1",
+        "x-requested-with": "XMLHttpRequest",
+        "cache-control": "no-cache"
+    }
+    parameters = {
+        "_search": sku_id,
+        "shoeSize": size,
+        "gender": "men",
+        "market.lowestAsk": f"range({price_low}|{price_high})",
+    }
+    response = requests.get(url, headers=headers, params=parameters)
+    response.raise_for_status()
+    request_data = response.json()["Products"]
+
+    item = request_data[0]
+    return [{
+        "source": "StockX",
+        "price": item["market"]["lowestAsk"],
+        "condition": item["condition"],
+        "image": item["media"]["imageUrl"],
+        "url": "stockx.com/" + item["urlKey"]
+    }]
+
 
 def get_goat_data(search_query, shoe_size, price_low, price_high, page, page_len):
 	url = "https://2fwotdvm2o-dsn.algolia.net/1/indexes/product_variants_v2/query"
@@ -91,6 +122,35 @@ def get_goat_data(search_query, shoe_size, price_low, price_high, page, page_len
 		})
 	
 	return results
+
+def get_goat_item_data(sku_id, size, price_low, price_high):
+    url = "https://2fwotdvm2o-dsn.algolia.net/1/indexes/product_variants_v2/query"
+    params = {
+        "x-algolia-agent": "Algolia for vanilla JavaScript 3.25.1",
+        "x-algolia-application-id": "2FWOTDVM2O",
+        "x-algolia-api-key": "ac96de6fef0e02bb95d433d8d5c7038a"
+    }
+
+    post_json = {
+        "params": urlencode({
+            "query": sku_id,
+            "facetFilters": f"(product_category: shoes), (presentation_size: {size}), (single_gender: men)",
+            "numericFilters": f"[\"lowest_price_cents_usd>={price_low*100}\",\"lowest_price_cents_usd<={price_high*100}\"]",
+        })
+    }
+
+    response = requests.post(url, params=params, json=post_json)
+    response.raise_for_status()
+    request_data = response.json()['hits']
+
+    item = request_data[0]
+    return [{
+        'source': "Goat",
+        'price': int(item['lowest_price_cents']/100),
+        'condition': item['shoe_condition'],
+        'image': item['main_picture_url'],
+        'url': 'goat.com/sneakers/' + item['slug']
+    }]
 
 
 def get_flightclub_data(search_query, shoe_size, price_low, price_high, page, page_len):
@@ -137,6 +197,34 @@ def get_flightclub_data(search_query, shoe_size, price_low, price_high, page, pa
 		})
 
 	return results
+
+def get_flightclub_item_data(sku_id, size, price_low, price_high):
+    url = "https://2fwotdvm2o-dsn.algolia.net/1/indexes/product_variants_v2_flight_club/query"
+    params = {
+        "x-algolia-agent": "Algolia for vanilla JavaScript (lite) 3.32.0;react-instantsearch 5.4.0;JS Helper 2.26.1",
+        "x-algolia-application-id": "2FWOTDVM2O",
+        "x-algolia-api-key": "ac96de6fef0e02bb95d433d8d5c7038a"
+    }
+    post_json = {
+        "params": urlencode({
+            "query": sku_id,
+            "facetFilters": f'[["size_us_men:{size}"]]',
+            "numericFilters": f"[\"lowest_price_cents_usd>={price_low*100}\",\"lowest_price_cents_usd<={price_high*100}\"]"
+        })
+    }
+
+    response = requests.post(url, params=params, json=post_json)
+    response.raise_for_status()
+    request_data = response.json()['hits']
+
+    item = request_data[0]
+    return [{
+        'source': "Flight Club",
+        'price': int(item['lowest_price_cents']/100),
+        'condition': item['shoe_condition'],
+        'image': item['main_picture_url'],
+        'url': 'flightclub.com/' + item['slug']
+    }]
 
 
 def get_grailed_data(search_query, shoe_size, price_low, price_high, sort_by, page, page_len):
@@ -206,6 +294,59 @@ def get_grailed_data(search_query, shoe_size, price_low, price_high, sort_by, pa
 	
 	return results
 
+def get_grailed_item_data(sku_id, size, price_low, price_high, max_items):
+    url = "https://mnrwefss2q-dsn.algolia.net/1/indexes/*/queries"
+    params = {
+        "x-algolia-agent": "Algolia for JavaScript (3.35.1); Browser",
+        "x-algolia-application-id": "MNRWEFSS2Q",
+        "x-algolia-api-key": "a3a4de2e05d9e9b463911705fb6323ad"
+    }
+
+    sort_by = {
+        "default": "Listing_production",
+        "trending": "Listing_by_heat_production",
+        "popular": "Listing_by_followers_production",
+        "new": "Listing_by_date_added_production",
+        "price_low": "Listing_by_low_price_production",
+        "price_high": "Listing_by_high_price_production",
+    }
+
+    post_json = {
+        "requests": [{
+            "indexName": sort_by["price_low"],
+            "params": urlencode({
+                "query": sku_id,
+                "facetFilters": f"[[\"category_size:footwear.{size}\"], [\"category_path:footwear.hitop_sneakers\", \"category_path:footwear.lowtop_sneakers\"]]",
+                "numericFilters": f"[\"price_i>={price_low}\",\"price_i<={price_high}\"]",
+            })
+        }]
+    }
+
+    response = requests.post(url, params=params, json=post_json)
+    response.raise_for_status()
+    request_data = response.json()["results"][0]["hits"]
+
+    results = []
+    for idx, item in enumerate(request_data):
+        if idx >= max_items: break
+        results.append({
+            "id": int(item['id']),
+            "source": "Grailed",
+            "price": item['price'],
+            "condition": item['condition'],
+            "shipping_us": item['shipping']['us']['amount'] if item['shipping']['us']['enabled'] else None,
+            "shipping_ca": item['shipping']['ca']['amount'] if item['shipping']['ca']['enabled'] else None,
+            "shipping_uk": item['shipping']['uk']['amount'] if item['shipping']['uk']['enabled'] else None,
+            "shipping_eu": item['shipping']['eu']['amount'] if item['shipping']['eu']['enabled'] else None,
+            "shipping_asia": item['shipping']['asia']['amount'] if item['shipping']['asia']['enabled'] else None,
+            "shipping_au": item['shipping']['au']['amount'] if item['shipping']['au']['enabled'] else None,
+            "shipping_other": item['shipping']['other']['amount'] if item['shipping']['other']['enabled'] else None,
+            "image": item['cover_photo']['url'],
+            "url": "grailed.com/listings/" + str(item['id'])
+        })
+
+    return results
+
 
 def get_sneakercon_data(search_query, shoe_size, price_low, price_high, page, page_len):
 	url = "https://war6i72q7j.execute-api.us-east-1.amazonaws.com/prod/public/marketplace/all"
@@ -262,3 +403,41 @@ def get_sneakercon_data(search_query, shoe_size, price_low, price_high, page, pa
 
 	return results
 
+def get_sneakercon_item_data(sku_id, size, price_low, price_high):
+    url = "https://war6i72q7j.execute-api.us-east-1.amazonaws.com/prod/public/marketplace/all"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
+        "origin": "https://sneakercon.com",
+        "referer": "https://sneakercon.com/",
+        "accept": "application/json",
+        "sec-ch-ua-mobile": "?0",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site"
+    }
+    parameters = {
+        "search": sku_id,
+        "size": size,
+        "limit": "1",
+        "isNew": "true"
+    }
+
+    response = requests.get(url, headers=headers, params=parameters)
+    response.raise_for_status()
+    request_data = response.json()
+
+    item = request_data[0]
+    prices_url = f"https://war6i72q7j.execute-api.us-east-1.amazonaws.com/prod/public/bid/new/makeofferdetails/{item['id']}"
+    price_data = requests.get(prices_url, headers=headers).json()
+    item_price = None
+    for p in price_data:
+        if p['size'] == str(size):
+            item_price = p['salePrice']
+
+    return [{
+        "source": 'Sneaker Con',
+        "price": item_price,
+        "condition": 'New',
+        "image": item['pictures'][0],
+        "url": "sneakercon.com/product/" + str(item['id']) + '-' + item['nickname'].replace(' ', '-')
+    }]
