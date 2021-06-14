0a2613e920cf56b0b658f95f045e63ce5b6fa058
Jason Thomo <amjbvic@gmail.com>
2021-06-14 11:07:59 -0700

    add currency conversion endpoint in api

--- API/application.py
+++ API/application.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 from scrapers import *
+from cachetools import cached, TTLCache
+from forex_python.converter import CurrencyRates
 
 application = Flask(__name__)
 cors = CORS(application, resources={r"*": {"origins": "*"}})
@@ -15,6 +17,8 @@ parser.add_argument('search', type=str, default='')
 parser.add_argument('size', type=str, default='10')
 parser.add_argument('ship_to', type=str, default='US')
 parser.add_argument('page', type=int, default=1)
+parser.add_argument('from_curr', type=str, default='USD')
+parser.add_argument('to_curr', type=str)
 
 
 def bad_request():
@@ -63,10 +67,24 @@ class ItemListings(Resource):
 			return response if response else bad_request()
 
 
+@cached(cache=TTLCache(maxsize=100, ttl=18000))
+def currency_rate(from_curr, to_curr):
+	c = CurrencyRates()
+	return c.get_rate(from_curr, to_curr)
+
+class CurrencyRate(Resource):
+	def get(self):
+		args = parser.parse_args()
+		from_curr = args['from_curr']
+		to_curr = args['to_curr']
+		return currency_rate(from_curr, to_curr)
+
+
 # Set API endpoints
 api.add_resource(Browse, '/browse')
 api.add_resource(LowestPrice, '/lowestprice')
 api.add_resource(ItemListings, '/itemlistings')
+api.add_resource(CurrencyRate, '/currencyrate')
 
 if __name__ == '__main__':
 	application.run(debug=True)        # local dev
