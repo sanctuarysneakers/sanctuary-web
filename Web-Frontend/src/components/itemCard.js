c9d09a2e848a26312a352d07a453daaadfed0eaf
raineakiyama-chen <#bacongriffin1bjj>
2021-07-13 14:40:31 -0400

    browse page responsiveness

--- Web-Frontend/src/components/itemCard.js
+++ Web-Frontend/src/components/itemCard.js
@@ -10,23 +10,42 @@ export default function ItemCard({ data }) {
     }
 
     return (
-        <div className='item-card' onClick={clickHandler}>
-            <div className='item-card-wrapper'>
 
-                <div className='item-card-shoe'>
+        <div className='item-card' onClick={clickHandler}>
+            <div className='item-card-content'>
+                <div className='item-card-sneaker'>
                     <img src={data.imageThumbnail} loading='lazy' alt={data.model} />
                 </div>
 
                 <div className='item-card-text'>
-                    <h2>
-                        {data.model}
-                    </h2>
-                    <p>
-                        Learn More
-                    </p>
-                </div>
+                    <h2> {data.model} </h2>
 
+                    <p> Learn More </p>
+                </div>
             </div>
         </div>
+
+
+
+
+
+        // <div className='item-card' onClick={clickHandler}>
+        //     <div className='item-card-wrapper'>
+
+        //         <div className='item-card-shoe'>
+        //             <img src={data.imageThumbnail} loading='lazy' alt={data.model} />
+        //         </div>
+
+        //         <div className='item-card-text'>
+        //             <h2>
+        //                 {data.model}
+        //             </h2>
+        //             <p>
+        //                 Learn More
+        //             </p>
+        //         </div>
+
+        //     </div>
+        // </div>
     )
 }
