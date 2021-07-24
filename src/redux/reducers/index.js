2602b5093bde9897d4a4b92df0ce5be8f3f31324
Jason Thomo <amjbvic@gmail.com>
2021-07-23 23:19:54 -0700

    removed old searchbar

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -6,12 +6,10 @@ import itemListingsReducer from './itemListingsReducer'
 import filterReducer from './filterReducer'
 import currencyReducer from './currencyReducer'
 import aboutModalVisibleReducer from './aboutModalVisibleReducer'
-import searchBarCollapseReducer from './searchBarCollapseReducer'
 import recordSplashHeightReducer from './recordSplashHeightReducer'
 import hamburgerModalVisibleReducer from './hamburgerModalVisibleReducer'
 import userReducer from './userReducer'
 import deleteModalVisibleReducer from './deleteModalVisibleReducer'
-import searchBarVisibleReducer from './searchBarVisibleReducer'
 import locationReducer from './locationReducer'
 import showfilterreducer from './showFilterReducer'
 import updateSizeReducer from './updateSizeReducer'
@@ -26,8 +24,6 @@ const globalReducer = combineReducers({
     itemInfo: itemInfoReducer,
     itemPrices: itemPricesReducer,
     itemListings: itemListingsReducer,
-    searchBarVisible: searchBarVisibleReducer,
-    isSearchBarCollapsed: searchBarCollapseReducer,
     splashHeight: recordSplashHeightReducer,
     hamburgerModalVisible: hamburgerModalVisibleReducer,
     user: userReducer,
