9435079530527ccc02869cd95d7ffd9dc692d94c
Jason Thomo <amjbvic@gmail.com>
2022-03-16 13:42:27 -0700

    basic portfolio page functionality

--- src/components/Other/portfolio.js
+++ src/components/Other/portfolio.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 export default function Portfolio() {
 
-    
+	const user = useSelector(state => state.user)
 
-    return (
-        <div>
-        </div>
-    )
+	let [data, setData] = useState([])
+
+	async function getData() {
+		const url = `https://hdwj2rvqkb.us-west-2.awsapprunner.com/accounts/portfolio/get?user_id=${user.uid}`
+		const response = await fetch(url)
+		const data = await response.json()
+		setData(data)
+	}
+
+	useEffect(() => {
+		getData()
+	}, [])
+
+	console.log(data)
+
+	return (
+		<div>{data.map((d) => {
+			return (
+				<div>
+					<h4>Item SKU: {d.sku}</h4>
+					<p>{d.add_date}</p>
+					<p>Size: {d.size}</p>
+					<p>Price: ${d.price}</p>
+					<br/>
+				</div>
+			)
+		})}</div>
+	)
 }
