d2a816d7ba6446c43b1487a62c39efbe2e23e19e
EricHasegawa <eric.hasegawa@mail.utoronto.ca>
2022-03-24 16:43:47 -0400

    set up basic portfolio data on frontend

--- src/components/portfolioCard.js
+++ src/components/portfolioCard.js
@@ -3,11 +3,20 @@ import React from 'react'
 export default function PortfolioCard({ data }) {
     return (
         <div className='portfolio-card'>
-            <h4>Item SKU: {data.sku}</h4>
-            <p>{data.add_date}</p>
+            <div className='portfolio-card-content'>
+                <div className='portfolio-card-sneaker'>
+                    <img src={data.itemInfo[0].image} loading='lazy' alt={data.itemInfo[0].modelName} />
+                </div>
+            <h4>{data.itemInfo[0].modelName}</h4>
+            <p>Added on: {data.add_date}</p>
             <p>Size: {data.size}</p>
-            <p>Price: ${data.price}</p>
-            <br/>
+            <p>Original price: ${data.price}</p>
+            <p>Current price: ${data.currPrices[0][0].price}</p>
+            <p> Price change: ${data.currPrices[0][0].price - data.price} </p>
+            <p> Percent change: {Math.round(((data.currPrices[0][0].price - data.price)/data.price) * 100)}%</p>
+
+
+            </div>
         </div>
     )
 }
