31323bdab1d6c4c1d76f99e72546ccab160bd82f
raineking <raine.akiyamachen@gmail.com>
2022-05-19 00:17:11 -0400

    Dynamic list regrouping and styling

--- src/components/Item/item.js
+++ src/components/Item/item.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet'
 import { HelmetProvider } from 'react-helmet-async'
 import { useParams, useLocation } from 'react-router'
 import { Link } from 'react-router-dom'
@@ -15,9 +15,9 @@ import ItemNoResults from './itemNoResults'
 import Footer from '../Other/footer'
 import { showSocialsModal } from '../../redux/actions'
 import { ReactComponent as Share } from '../../assets/images/share.svg'
-import { websiteLogoMapGrey, currencySymbolMap }  from '../../assets/constants'
+import { websiteLogoMapGrey, currencySymbolMap } from '../../assets/constants'
 import DynamicList from '../Other/dynamicList'
-import RecommendedCard from './recommendedCard';
+import RecommendedCard from './recommendedCard'
 
 export default function Item() {
 
@@ -30,9 +30,9 @@ export default function Item() {
     const navLocation = useLocation()
     let passedData = navLocation.itemInfo ? navLocation.itemInfo : null
     useAPICall('getitem', {
-        itemKey: itemKey.toLowerCase() === itemKey ? itemKey: decodeURIComponent(itemKey.replaceAll('-', ' ')), //differentiates between urlkey and sku
-        size: size, 
-        gender: gender, 
+        itemKey: itemKey.toLowerCase() === itemKey ? itemKey : decodeURIComponent(itemKey.replaceAll('-', ' ')), //differentiates between urlkey and sku
+        size: size,
+        gender: gender,
         fromBrowse: passedData
     })
 
@@ -56,7 +56,7 @@ export default function Item() {
     )
 
     const clickHandler = () => {
-        window.analytics.track(`item_buy_new_clicked`, {sku: decodeURIComponent(itemKey), gender: gender, model: itemInfo.modelName});
+        window.analytics.track(`item_buy_new_clicked`, { sku: decodeURIComponent(itemKey), gender: gender, model: itemInfo.modelName });
     }
 
     /* eslint-disable jsx-a11y/anchor-is-valid */
@@ -69,7 +69,7 @@ export default function Item() {
                     <meta property="og:image" content={itemInfo.image} />
                 </Helmet>
             </HelmetProvider>
-            
+
             <div className='item-sneaker'>
                 <div className='item-sneaker-wrapper'>
                     <div className='item-sneaker-actions'>
@@ -77,7 +77,7 @@ export default function Item() {
                             <Share />
                         </div>
                     </div>
-                
+
                     <div className='item-sneaker-content'>
                         <div className='item-sneaker-image'>
                             <img src={itemInfo.image} alt='sneaker' />
@@ -87,14 +87,14 @@ export default function Item() {
                             <div className='item-sneaker-text'>
                                 {pricesLoading && <ItemLoader version={'source'} />}
                                 {!pricesLoading && <div className='item-sneaker-source'>
-                                    {itemPrices.length ? 
-                                    <div className={`item-sneaker-site ${itemPrices[0].source}`}>
-                                        <img 
-                                            src={websiteLogoMapGrey[itemPrices[0].source]} alt='website logo' 
-                                        />
-                                    </div> 
-                                    : 
-                                    <div className='item-sneaker-source-none'></div>}
+                                    {itemPrices.length ?
+                                        <div className={`item-sneaker-site ${itemPrices[0].source}`}>
+                                            <img
+                                                src={websiteLogoMapGrey[itemPrices[0].source]} alt='website logo'
+                                            />
+                                        </div>
+                                        :
+                                        <div className='item-sneaker-source-none'></div>}
                                 </div>}
 
                                 <div className='item-sneaker-model'>
@@ -103,27 +103,27 @@ export default function Item() {
 
                                 {pricesLoading && <ItemLoader version={'info'} />}
                                 {!pricesLoading && <div className='item-sneaker-price-details'>
-                                    {itemPrices.length ? 
-                                    
-                                    <Link to={{ pathname: itemPrices[0].url }} className="hidden-link" target="_blank" rel="noopener noreferrer" onClick={clickHandler} onContextMenu={clickHandler}> 
-                                        <div className='item-sneaker-price'>
-                                            <h2>
-                                                Buy New {currencySymbolMap[currency]}{itemPrices[0].price}
-                                            </h2>
-                                        </div>
-                                    </Link>
-                                    : 
-                                    <a>
-                                        <div className='item-sneaker-price none'>
-                                            <h2>
-                                                No Results
-                                            </h2>
-                                        </div>
-                                    </a>}
+                                    {itemPrices.length ?
+
+                                        <Link to={{ pathname: itemPrices[0].url }} className="hidden-link" target="_blank" rel="noopener noreferrer" onClick={clickHandler} onContextMenu={clickHandler}>
+                                            <div className='item-sneaker-price'>
+                                                <h2>
+                                                    Buy New {currencySymbolMap[currency]}{itemPrices[0].price}
+                                                </h2>
+                                            </div>
+                                        </Link>
+                                        :
+                                        <a>
+                                            <div className='item-sneaker-price none'>
+                                                <h2>
+                                                    No Results
+                                                </h2>
+                                            </div>
+                                        </a>}
 
                                     <SizeFilter gender={gender} />
                                     {sizeModalVisible && <SizeModal gender={gender} />}
-                                    {socialsModalVisible && <SocialsModal itemName={itemInfo.modelName} price={`${currencySymbolMap[currency]}${itemPrices[0].price}`} url={window.location.href} image={itemInfo.iamge}/> }
+                                    {socialsModalVisible && <SocialsModal itemName={itemInfo.modelName} price={`${currencySymbolMap[currency]}${itemPrices[0].price}`} url={window.location.href} image={itemInfo.iamge} />}
                                 </div>}
                             </div>
                         </div>
@@ -147,19 +147,37 @@ export default function Item() {
                     <div className='item-more-listings'>
                         <h6> More Listings </h6>
 
-                        <div className='item-more-listings-rows'>
+                        {/* <div className='item-more-listings-rows'>
                             {listingsLoading && <ItemLoader version={'listings'} />}
+
                             {!listingsLoading && <div className='item-more-listings-rows-content'>
-                                {itemListings.length ? <DynamicList items={listingComponents} initialLength={5}/> : <ItemNoResults version={'listings'} />}
+                                {itemListings.length ? <DynamicList name={'item-more-listings-rows-content'} items={listingComponents} initialLength={5}/> : <ItemNoResults version={'listings'} />}
                             </div>}
-                        </div>
+                        </div> */}
+
+                        {listingsLoading ?
+                            <div className='item-more-listings-rows'>
+                                <ItemLoader version={'listings'} />
+                            </div>
+                            :
+                            <div>
+                                {itemListings.length ?
+                                    <DynamicList 
+                                        name={'item-more-listings-rows'}
+                                        items={listingComponents} 
+                                        initialLength={5} />
+                                    : 
+                                    <ItemNoResults version={'listings'} />
+                                }
+                            </div>
+                        }
                     </div>
 
-                    <div className='item-more-listings'> 
+                    <div className='item-more-listings'>
                         <h6> Related Models</h6>
-                            {!relatedLoading && relatedItems.length !== 0 && 
-                                <div> {relatedItems.map((item) => <RecommendedCard data={item} />)}</div>
-                            }
+                        {!relatedLoading && relatedItems.length !== 0 &&
+                            <div> {relatedItems.map((item) => <RecommendedCard data={item} />)}</div>
+                        }
                     </div>
                 </div>
             </div>
--- src/components/Other/dynamicList.js
+++ src/components/Other/dynamicList.js
@@ -1,9 +1,10 @@
-// component used to wrap a list of elements to allow the user to choose to see more/less items 
 import React, { useState, useEffect } from 'react' 
 
-export default function DynamicList({ items, initialLength }) {
+// component used to wrap a list of elements to allow the user to choose to see more/less items 
+export default function DynamicList({ name, items, initialLength }) {
 
     let useShowMore= (items && items.length > initialLength)
+
     const [showMore, setShowMore] = useState(true)
     const [displayItems, setDisplayitems] = useState(items)
 
@@ -17,9 +18,16 @@ export default function DynamicList({ items, initialLength }) {
     }, [showMore])
 
     return (
-        <div>
-            {displayItems}
-            {useShowMore && <button onClick={() => setShowMore(!showMore)}> {showMore ? "Show More" : "Show Less"} </button> }
+        <div className='dynamic-list'>
+            <div className={name}>
+                {displayItems}
+            </div>
+            
+            {useShowMore &&
+                <button className='dynamic-list-button' onClick={() => setShowMore(!showMore)}>
+                    {showMore ? "See All" : "See Less"}
+                </button>
+            }
         </div>
     )
 }
