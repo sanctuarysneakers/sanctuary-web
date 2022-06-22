69a237c7c86568e72e6c9834789e834810ecc3ac
jchua24 <winwin.chua@gmail.com>
2022-06-21 21:57:51 -0400

    misc

--- src/components/Browse/browse.js
+++ src/components/Browse/browse.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet'
 import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router'
