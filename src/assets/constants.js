2123c00381bf9d3cdeffd8e7c06ddafec4501703
jchua24 <winwin.chua@gmail.com>
2022-06-28 21:44:43 -0400

    update constants

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -101,13 +101,16 @@ export const brandOptions = [
     {value: 'balenciaga', label: 'Balenciaga'},
     {value: 'converse', label: 'Converse'},
     {value: 'fila', label: 'Fila'},
-    {value: 'jordan', label: 'Jordan'},
+    {value: 'gucci', label: 'Gucci'},
+    {value: 'air jordan', label: 'Jordan'},
     {value: 'new balance', label: 'New Balance'},
     {value: 'nike', label: 'Nike'},
-    {value: 'off-white', label: 'Off-White'}, 
+    {value: 'off white', label: 'Off-White'}, 
     {value: 'puma', label: 'Puma'},
     {value: 'reebok', label: 'Reebok'},
     {value: 'saucony', label: 'Saucony'},
+    {value: 'supreme', label: 'Supreme'},
+    {value: 'under armour', label: 'Under Armour'}, 
     {value: 'vans', label: 'Vans'},
     {value: 'yeezy', label: 'Yeezy'}
 ]
@@ -116,46 +119,24 @@ export const sizeTypeOptions = [
     {value: null, label: 'All'},
     {value: 'men', label: 'Men'},
     {value: 'women', label: 'Women'}, 
-    // {value: 'child', label: 'Child'}
+    {value: 'youth', label: 'Youth'}, 
+    {value: 'infant', label: 'Infant'}
 ]
 
+// sort options
 export const sortOptions = [
-    {value: null, label: 'Featured'},
-    {value: '{"sort": "release_date"}', label: 'Newest Releases'},
-    {value: '{"sort": "most-active"}', label: 'Most Popular'},
-    {value: '{"sort": "lowest_ask", "order": "ASC"}', label: 'Price: Low to High'},
-    {value: '{"sort": "average_deadstock_price", "order": "DESC"}', label: 'Price: High to Low'}
+    {value: null, label: 'All'},
+    {value: '{"sort": "date_added", "order": "descending"}', label: 'Newest Releases'},
+    {value: '{"sort": "relevance", "order": "descending"}', label: 'Most Popular'},
+    {value: '{"sort": "lowest_price_cents", "order": "ascending"}', label: 'Price: Low to High'},
+    {value: '{"sort": "lowest_price_cents", "order": "descending"}', label: 'Price: High to Low'}
 ]
 
 // browse - multi-select filter options 
+export const releaseYearOptions = [...(function*(){for(let n=new Date().getFullYear();n>=2000;)yield({"value": n.toString(), "label": n.toString()}), n--})()]
 
-export const releaseYearOptions = [
-    {value: '2022', label: '2022'},
-    {value: '2021', label: '2021'},
-    {value: '2020', label: '2020'},
-    {value: '2019', label: '2019'},
-    {value: '2018', label: '2018'},
-    {value: '2017', label: '2017'},
-    {value: '2016', label: '2016'},
-    {value: '2015', label: '2015'},
-    {value: '2014', label: '2014'},
-    {value: '2013', label: '2013'},
-    {value: '2012', label: '2012'},
-    {value: '2011', label: '2011'},
-    {value: '2010', label: '2010'},
-    {value: '2009', label: '2009'},
-    {value: '2008', label: '2008'},
-    {value: '2007', label: '2007'},
-    {value: '2006', label: '2006'},
-    {value: '2005', label: '2005'},
-    {value: '2004', label: '2004'},
-    {value: '2003', label: '2003'},
-    {value: '2002', label: '2002'},
-    {value: '2001', label: '2001'},
-    {value: '2000', label: '2000'},
-    {value: 'lt-2000', label: '<2000'}
-]
 
+//TODO
 export const priceOptions = [
     {value: 'lte-100', label: 'Under $100'},
     {value: 'range(100|200)', label: '$100-$200'},
